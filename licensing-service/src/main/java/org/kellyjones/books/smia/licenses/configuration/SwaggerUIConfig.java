73374e4f273150e8b8f28efc007d70ff65f0ac2f
Stuart Jones <stu@kellyjones.org>
2020-08-27 21:58:37 -0400

    Slightly more selective Swagger docs

--- licensing-service/src/main/java/org/kellyjones/books/smia/licenses/configuration/SwaggerUIConfig.java
+++ licensing-service/src/main/java/org/kellyjones/books/smia/licenses/configuration/SwaggerUIConfig.java
@@ -16,7 +16,7 @@ public class SwaggerUIConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("org.kellyjones"))
                 .paths(PathSelectors.any())
                 .build();
     }
