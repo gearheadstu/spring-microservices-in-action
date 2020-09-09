2e7e30b1edd7af73a9d7435e28b7953af14e319f
Stuart Jones <stu@kellyjones.org>
2020-09-09 18:15:32 -0400

    Made the license_service a client of the config_service

--- licensing-service/src/main/java/org/kellyjones/books/smia/licenses/Application.java
+++ licensing-service/src/main/java/org/kellyjones/books/smia/licenses/Application.java
@@ -1,8 +1,10 @@
 package org.kellyjones.books.smia.licenses;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAutoConfiguration
 @SpringBootApplication
 public class Application {
 
